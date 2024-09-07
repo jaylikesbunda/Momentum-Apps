fe1a7f6d16a9f08bb9ee4c6d4d29748d0e25335a
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2024-09-07 08:50:38 +0200

    add adc example

--- examples/flipperzero_gpio_interrupt_test.py
+++ examples/flipperzero_gpio_interrupt_test.py
@@ -8,10 +8,10 @@ f0.gpio_init_pin(f0.GPIO_PIN_PC1, f0.GPIO_MODE_INTERRUPT_RISE, f0.GPIO_PULL_UP, 
 
 @f0.on_gpio
 def on_gpio(pin):
-    if pin == f0.GPIO_PIN_PC0:
-        f0.gpio_set_pin(f0.GPIO_PIN_PA7, True)
-    if pin == f0.GPIO_PIN_PC1:
-        f0.gpio_set_pin(f0.GPIO_PIN_PA7, False)
+  if pin == f0.GPIO_PIN_PC0:
+    f0.gpio_set_pin(f0.GPIO_PIN_PA7, True)
+  if pin == f0.GPIO_PIN_PC1:
+    f0.gpio_set_pin(f0.GPIO_PIN_PA7, False)
 
 for _ in range(1, 1500):
   time.sleep_ms(10)
