60fd3d6587107acf7e991e57c17c70159b08c610
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2024-09-26 17:04:09 +0200

    library update

---
+++ mp_flipper_repl_helper.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stddef.h>
+
+#include "py/repl.h"
+#include "py/mpprint.h"
+
+inline bool mp_flipper_repl_continue_with_input(const char* input) {
+    return mp_repl_continue_with_input(input);
+}
+
+inline size_t mp_flipper_repl_autocomplete(const char* str, size_t len, const mp_print_t* print, const char** compl_str) {
+    return mp_repl_autocomplete(str, len, print, compl_str);
+}
