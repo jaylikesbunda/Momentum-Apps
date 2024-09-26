dbed8d44acf131bc35cdbfd5312109fafd11e8df
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2024-09-26 16:26:16 +0200

    library update

--- mp_flipper_config_fap.h
+++ mp_flipper_config_fap.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define MP_FLIPPER_REPL
-#define MP_FLIPPER_COMPILER
-#define MP_FLIPPER_RUNTIME
-#define MP_FLIPPER_SPLIT_HEAP
-#define MP_FLIPPER_ERROR_REPORTING
+#define MP_FLIPPER_REPL (1)
+#define MP_FLIPPER_COMPILER (1)
+#define MP_FLIPPER_RUNTIME (1)
+#define MP_FLIPPER_SPLIT_HEAP (1)
+#define MP_FLIPPER_ERROR_REPORTING (1)
 
 #include "mp_flipper_config.h"
