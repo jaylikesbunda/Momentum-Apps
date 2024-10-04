739f1b38e2a9dc501da1a9dee702c5d175f70699
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2024-10-04 07:25:50 +0200

    library update

--- mp_flipper_logging.c
+++ mp_flipper_logging.c
@@ -4,56 +4,85 @@
 
 #include "py/obj.h"
 
-extern const mp_obj_type_t mp_flipper_logger_type;
+#include "mp_flipper_logging.h"
 
-typedef struct _mp_flipper_logger_t {
-    mp_obj_base_t base;
-    mp_obj_t name;
-} mp_flipper_logger_t;
+static mp_obj_t mp_flipper_logging_log_internal(uint8_t level, size_t n_args, const mp_obj_t* args) {
+    if(n_args < 1 || level < mp_flipper_log_get_level()) {
+        return mp_const_none;
+    }
 
-static mp_obj_t mp_flipper_logger_debug(size_t n_args, const mp_obj_t* args) {
+    mp_obj_t values = n_args > 1 ? mp_obj_new_tuple(n_args - 1, &args[1]) : mp_const_empty_tuple;
+    mp_obj_t message = mp_obj_str_binary_op(MP_BINARY_OP_MODULO, args[0], values);
+
+    mp_flipper_log(level, mp_obj_str_get_str(message));
+
+    return mp_const_none;
+}
+
+static mp_obj_t mp_flipper_logging_get_level() {
+    return mp_obj_new_int_from_uint(mp_flipper_log_get_level());
+}
+static MP_DEFINE_CONST_FUN_OBJ_0(mp_flipper_logging_get_level_obj, mp_flipper_logging_get_level);
+
+static mp_obj_t mp_flipper_logging_set_level(mp_obj_t level) {
+    mp_flipper_log_set_level(mp_obj_get_int(level));
+
+    return mp_const_none;
+}
+static MP_DEFINE_CONST_FUN_OBJ_1(mp_flipper_logging_set_level_obj, mp_flipper_logging_set_level);
+
+static mp_obj_t mp_flipper_logging_log(size_t n_args, const mp_obj_t* args) {
     if(n_args < 2) {
         return mp_const_none;
     }
 
-    mp_flipper_logger_t* logger = MP_OBJ_TO_PTR(args[0]);
+    uint8_t level = mp_obj_get_int(args[0]);
 
-    mp_obj_t values = mp_obj_new_tuple(n_args - 2, &args[2]);
-    mp_obj_t message = mp_obj_str_binary_op(MP_BINARY_OP_MODULO, args[1], values);
-
-    mp_flipper_log_debug(mp_obj_str_get_str(logger->name), mp_obj_str_get_str(message));
-
-    return mp_const_none;
+    return mp_flipper_logging_log_internal(level, n_args - 1, &args[1]);
 }
-static MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(mp_flipper_logger_debug_obj, 2, 4, mp_flipper_logger_debug);
+static MP_DEFINE_CONST_FUN_OBJ_VAR(mp_flipper_logging_log_obj, 2, mp_flipper_logging_log);
 
-static mp_obj_t mp_flipper_logger_name(mp_obj_t self) {
-    mp_flipper_logger_t* logger = MP_OBJ_TO_PTR(self);
-
-    return logger->name;
+static mp_obj_t mp_flipper_logging_trace(size_t n_args, const mp_obj_t* args) {
+    return mp_flipper_logging_log_internal(MP_FLIPPER_LOG_LEVEL_TRACE, n_args, args);
 }
-static MP_DEFINE_CONST_FUN_OBJ_1(mp_flipper_logger_name_obj, mp_flipper_logger_name);
+static MP_DEFINE_CONST_FUN_OBJ_VAR(mp_flipper_logging_trace_obj, 1, mp_flipper_logging_trace);
 
-static const mp_map_elem_t mp_flipper_logger_locals_dict_table[] = {
-    {MP_OBJ_NEW_QSTR(MP_QSTR_name), MP_ROM_PTR(&mp_flipper_logger_name_obj)},
-    {MP_ROM_QSTR(MP_QSTR_debug), MP_ROM_PTR(&mp_flipper_logger_debug_obj)},
-};
-static MP_DEFINE_CONST_DICT(mp_flipper_logger_locals_dict, mp_flipper_logger_locals_dict_table);
-
-MP_DEFINE_CONST_OBJ_TYPE(mp_flipper_logger_type, MP_QSTR_Logger, MP_TYPE_FLAG_NONE, locals_dict, &mp_flipper_logger_locals_dict);
-
-static mp_obj_t mp_flipper_logging_get_logger(mp_obj_t name) {
-    mp_flipper_logger_t* logger = mp_obj_malloc(mp_flipper_logger_t, &mp_flipper_logger_type);
-
-    logger->name = name;
-
-    return MP_OBJ_FROM_PTR(logger);
+static mp_obj_t mp_flipper_logging_debug(size_t n_args, const mp_obj_t* args) {
+    return mp_flipper_logging_log_internal(MP_FLIPPER_LOG_LEVEL_DEBUG, n_args, args);
 }
-static MP_DEFINE_CONST_FUN_OBJ_1(mp_flipper_logging_get_logger_obj, mp_flipper_logging_get_logger);
+static MP_DEFINE_CONST_FUN_OBJ_VAR(mp_flipper_logging_debug_obj, 1, mp_flipper_logging_debug);
+
+static mp_obj_t mp_flipper_logging_info(size_t n_args, const mp_obj_t* args) {
+    return mp_flipper_logging_log_internal(MP_FLIPPER_LOG_LEVEL_INFO, n_args, args);
+}
+static MP_DEFINE_CONST_FUN_OBJ_VAR(mp_flipper_logging_info_obj, 1, mp_flipper_logging_info);
+
+static mp_obj_t mp_flipper_logging_warn(size_t n_args, const mp_obj_t* args) {
+    return mp_flipper_logging_log_internal(MP_FLIPPER_LOG_LEVEL_WARN, n_args, args);
+}
+static MP_DEFINE_CONST_FUN_OBJ_VAR(mp_flipper_logging_warn_obj, 1, mp_flipper_logging_warn);
+
+static mp_obj_t mp_flipper_logging_error(size_t n_args, const mp_obj_t* args) {
+    return mp_flipper_logging_log_internal(MP_FLIPPER_LOG_LEVEL_ERROR, n_args, args);
+}
+static MP_DEFINE_CONST_FUN_OBJ_VAR(mp_flipper_logging_error_obj, 1, mp_flipper_logging_error);
 
 static const mp_rom_map_elem_t mp_module_logging_globals_table[] = {
     {MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_logging)},
-    {MP_ROM_QSTR(MP_QSTR_getLogger), MP_ROM_PTR(&mp_flipper_logging_get_logger_obj)},
+    {MP_ROM_QSTR(MP_QSTR_getLevel), MP_ROM_PTR(&mp_flipper_logging_get_level_obj)},
+    {MP_ROM_QSTR(MP_QSTR_setLevel), MP_ROM_PTR(&mp_flipper_logging_set_level_obj)},
+    {MP_ROM_QSTR(MP_QSTR_trace), MP_ROM_PTR(&mp_flipper_logging_trace_obj)},
+    {MP_ROM_QSTR(MP_QSTR_debug), MP_ROM_PTR(&mp_flipper_logging_debug_obj)},
+    {MP_ROM_QSTR(MP_QSTR_info), MP_ROM_PTR(&mp_flipper_logging_info_obj)},
+    {MP_ROM_QSTR(MP_QSTR_warn), MP_ROM_PTR(&mp_flipper_logging_warn_obj)},
+    {MP_ROM_QSTR(MP_QSTR_error), MP_ROM_PTR(&mp_flipper_logging_error_obj)},
+    {MP_ROM_QSTR(MP_QSTR_log), MP_ROM_PTR(&mp_flipper_logging_log_obj)},
+    {MP_ROM_QSTR(MP_QSTR_TRACE), MP_ROM_INT(MP_FLIPPER_LOG_LEVEL_TRACE)},
+    {MP_ROM_QSTR(MP_QSTR_DEBUG), MP_ROM_INT(MP_FLIPPER_LOG_LEVEL_DEBUG)},
+    {MP_ROM_QSTR(MP_QSTR_INFO), MP_ROM_INT(MP_FLIPPER_LOG_LEVEL_INFO)},
+    {MP_ROM_QSTR(MP_QSTR_WARN), MP_ROM_INT(MP_FLIPPER_LOG_LEVEL_WARN)},
+    {MP_ROM_QSTR(MP_QSTR_ERROR), MP_ROM_INT(MP_FLIPPER_LOG_LEVEL_ERROR)},
+    {MP_ROM_QSTR(MP_QSTR_NONE), MP_ROM_INT(MP_FLIPPER_LOG_LEVEL_NONE)},
 };
 
 static MP_DEFINE_CONST_DICT(mp_module_logging_globals, mp_module_logging_globals_table);
--- mp_flipper_logging.h
+++ mp_flipper_logging.h
@@ -1,5 +1,14 @@
 #pragma once
 
-#include <stdio.h>
+#include <stdint.h>
 
-void mp_flipper_log_debug(const char* name, const char* message);
+#define MP_FLIPPER_LOG_LEVEL_TRACE (0)
+#define MP_FLIPPER_LOG_LEVEL_DEBUG (1)
+#define MP_FLIPPER_LOG_LEVEL_INFO (2)
+#define MP_FLIPPER_LOG_LEVEL_WARN (3)
+#define MP_FLIPPER_LOG_LEVEL_ERROR (4)
+#define MP_FLIPPER_LOG_LEVEL_NONE (5)
+
+uint8_t mp_flipper_log_get_level();
+void mp_flipper_log_set_level(uint8_t level);
+void mp_flipper_log(uint8_t level, const char* message);
