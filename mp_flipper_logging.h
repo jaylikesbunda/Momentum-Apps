71e48be14d770d73686b35f17b4bb42bfb1c5117
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2024-10-05 07:45:19 +0200

    library update

--- mp_flipper_logging.h
+++ mp_flipper_logging.h
@@ -3,12 +3,12 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-extern uint8_t MP_FLIPPER_LOG_LEVEL_TRACE;
-extern uint8_t MP_FLIPPER_LOG_LEVEL_DEBUG;
-extern uint8_t MP_FLIPPER_LOG_LEVEL_INFO;
-extern uint8_t MP_FLIPPER_LOG_LEVEL_WARN;
-extern uint8_t MP_FLIPPER_LOG_LEVEL_ERROR;
-extern uint8_t MP_FLIPPER_LOG_LEVEL_NONE;
+extern const uint8_t MP_FLIPPER_LOG_LEVEL_TRACE;
+extern const uint8_t MP_FLIPPER_LOG_LEVEL_DEBUG;
+extern const uint8_t MP_FLIPPER_LOG_LEVEL_INFO;
+extern const uint8_t MP_FLIPPER_LOG_LEVEL_WARN;
+extern const uint8_t MP_FLIPPER_LOG_LEVEL_ERROR;
+extern const uint8_t MP_FLIPPER_LOG_LEVEL_NONE;
 
 uint8_t mp_flipper_log_get_effective_level();
 bool mp_flipper_log_level_is_valid(uint8_t level);
